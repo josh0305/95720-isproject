296cf0084a95044a4960488f2eecaa97ff2dcaaf
Sherry Lu <hsuehil@andrew.cmu.edu>
2022-07-08 01:50:23 +0930

    sherry-0708
    - init commit
    - django framework
    - front-end web appearance

---
+++ isproject/home/static/js/tab.js
@@ -0,0 +1,14 @@
+// Tabs
+function openLink(evt, linkName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("tabContent");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+  }
+  document.getElementById(linkName).style.display = "block";
+  evt.currentTarget.className += " w3-red";
+}
